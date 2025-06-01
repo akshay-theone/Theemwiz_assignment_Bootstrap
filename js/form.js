8724e2fe0a1f1c3e4f5206bff65c5289d49b2e81
akshay-theone <gawdeakshay82@gmail.com>
2025-06-01 22:49:52 +0530

    Upd Name

--- js/form.js
+++ js/form.js
@@ -2,11 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('.needs-validation');
 
   form.addEventListener('submit', function (event) {
-    event.preventDefault(); // prevent default submit to avoid page reload
+    event.preventDefault();
     form.classList.add('was-validated');
 
     if (form.checkValidity()) {
-      // form is valid, you can submit or do other actions
       alert('Form submitted!');
       form.reset();
       form.classList.remove('was-validated');
